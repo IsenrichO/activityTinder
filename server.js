35598da8fb9cf45687a374782c8740b33f500f1a
Joshua Fong <jwfong89@gmail.com>
2016-09-22 15:16:17 -0700

    [dev] added server.js file and installed express
    added .gitignore file for node modules
    added package.json file for dependencies

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.all('/', (req, res) => {
+  res.send(`
+    <!DOCTYPE html >
+    <html>
+      <head>
+        <title>Activity Tinder</title>
+      </head>
+      <body>
+        <div>
+          <h1> This is for Activity Tinder</h1>
+        </div>
+      </body>
+    </html>
+    `)
+})
+
+
+app.listen(PORT, () => {
+  console.log('Server is listening of port ' + PORT);
+});
+
+
+
+
+
